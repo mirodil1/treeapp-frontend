298f1f41fc9daf209f3160788db88f3127715f8d
mirodil <ortikov.m97@gmail.com>
2022-12-07 13:33:38 +0500

    home page url

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -2,7 +2,7 @@
 	<header>
 		<nav class="container navbar is-transparent">
 			<div class="navbar-brand">
-				<a class="navbar-item is-size-3 has-text-success" href="http://localhost:8080/">
+				<a class="navbar-item is-size-3 has-text-success" href="daraxt.redapp.uz">
 					<i class="fas fa-tree">Daraxt</i>
 				</a>
 				<a class="navbar-item" href="https://bulma.io">
